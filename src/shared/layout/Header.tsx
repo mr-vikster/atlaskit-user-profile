891d8c0d4808ef361cebbdffcc08b47e16696f9e
Viktor Pokhvaleny <mrvikster42@gmail.com>
2021-02-07 10:19:19 +0100

    init create-reat-app with basic atlaskit header

---
+++ src/shared/layout/Header.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { AtlassianNavigation, ProductHome, PrimaryButton } from '@atlaskit/atlassian-navigation'
+import { AtlassianIcon, AtlassianLogo } from '@atlaskit/logo'
+
+const ProductHomeExample = () => (
+  <ProductHome icon={AtlassianIcon} logo={AtlassianLogo} siteTitle="User Profile" />
+);
+
+const Header = () => {
+  return (
+    <div>
+      <AtlassianNavigation
+        label="site"
+        primaryItems={[
+          <PrimaryButton>User Profile</PrimaryButton>,
+        ]}
+        renderProductHome={ProductHomeExample}
+      />
+    </div>
+  )
+}
+
+export default Header
---
+++ src/views/Root.tsx
@@ -0,0 +1,15 @@
+import React, { Fragment } from 'react'
+import Header from '../shared/layout/Header';
+import Footer from '../shared/layout/Footer';
+
+const Root = () => {
+  return (
+    <Fragment>
+      <Header />
+      switch
+      <Footer />
+    </Fragment>
+  )
+}
+
+export default Root
